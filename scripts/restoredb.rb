88d2ad0832f9f0edb17a56baa5995ae57735dd46
nada <nada.wagahai@gmail.com>
2018-06-05 12:43:03 +0900

    add script

---
+++ scripts/restoredb.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require './lib/option'
+require './lib/parser'
+require './lib/db/file'
+
+def main(args)
+  opt = Option.new(args)
+  records_dir = "%s/%s" % [opt.data_dir, opt.records_dir]
+
+  db = FileDB.new(opt.data_dir + "/db")
+  Dir["%s/*" % records_dir].each do |file|
+    parser = Parser.new
+
+    input = ""
+    open(file) {|f|
+      input = f.read
+    }
+
+    kifu = parser.parse! input
+    db.put_kifu(kifu)
+
+    kifu_id = kifu.kifu_id
+    filename = File.basename file
+    if kifu_id != filename
+      File.rename(file, "%s/%s" % [records_dir, kifu_id])
+    end
+  end
+end
+
+main(ARGV)
