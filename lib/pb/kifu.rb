280dd37dce67276d2d50a4860054112c6b71e34d
nada <nada.wagahai@gmail.com>
2018-06-05 16:10:49 +0900

    orderd pieces

--- lib/pb/kifu.rb
+++ lib/pb/kifu.rb
@@ -63,6 +63,47 @@ class Kifu::Pos
   end
 end
 
+class Kifu::Piece
+  def <=>(o)
+    c = [self, o]
+    r = cmp(o)
+    raise "Same piece" if r == 0
+    puts "%s => %s" % [c, r == -1]
+    r
+  end
+
+  def captured?
+    pos.nil? || self.pos.x == 0
+  end
+
+  def name
+    Kifu::Piece::Type.name(self.type)
+  end
+
+  private
+
+  def cmp(o)
+    if self.captured? && o.captured?
+      t = Kifu::Player::Order
+      a = t.resolve(o.order) <=> t.resolve(self.order)
+      if a == 0
+        t = Kifu::Piece::Type
+        return t.resolve(self.type) <=> t.resolve(o.type)
+      end
+      return a
+    elsif self.captured?
+      return self.order == :SECOND ? -1 : 1
+    elsif o.captured?
+      return o.order == :SECOND ? 1 : -1
+    end
+
+    py = self.pos.y <=> o.pos.y
+    return py if py != 0
+
+    self.pos.x <=> o.pos.x
+  end
+end
+
 module Kifu::Piece::Type
   PIECES = ["王", "金", "銀", "成銀", "桂", "成桂", "香", "成香", "角", "馬", "飛", "竜", "歩", "と"]
 
@@ -74,11 +115,26 @@ module Kifu::Piece::Type
   end
 end
 
+module Kifu::Player::Order
+  def self.label(sym)
+    case sym
+    when :FIRST
+      "☗"
+    when :SECOND
+      "☖"
+    else
+      raise "unknown order = %s" % sym
+    end
+  end
+end
+
 class Kifu::Step
   def move
-    return "投了" if finished
+    str = Kifu::Player::Order.label(self.player)
 
-    str = pos.to_code + Kifu::Piece::Type.name(piece)
+    return str + "投了" if finished
+
+    str << pos.to_code + Kifu::Piece::Type.name(piece)
     return str + "打" if putted
     str << "成" if promoted
     str + "(%d%d)" % prev.to_a
@@ -90,5 +146,61 @@ class Kifu::Step
     m, ts_s = timestamp_sec.divmod(60)
     ts_h, ts_m = m.divmod(60)
     "%02d:%02d:%02d / %02d:%02d:%02d" % [th_h, th_m, th_s, ts_h, ts_m, ts_s]
+  end
+end
+
+class Kifu::Board
+  def self.init
+    ps = []
+
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 1, y: 1), type: :KYOU, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 2, y: 1), type: :KEI, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 3, y: 1), type: :GIN, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 4, y: 1), type: :KIN, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 5, y: 1), type: :GYOKU, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 6, y: 1), type: :KIN, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 7, y: 1), type: :GIN, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 8, y: 1), type: :KEI, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 9, y: 1), type: :KYOU, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 2, y: 2), type: :KAKU, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 8, y: 2), type: :HISHA, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 1, y: 3), type: :FU, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 2, y: 3), type: :FU, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 3, y: 3), type: :FU, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 4, y: 3), type: :FU, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 5, y: 3), type: :FU, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 6, y: 3), type: :FU, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 7, y: 3), type: :FU, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 8, y: 3), type: :FU, order: :SECOND)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 9, y: 3), type: :FU, order: :SECOND)
+
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 1, y: 7), type: :FU, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 2, y: 7), type: :FU, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 3, y: 7), type: :FU, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 4, y: 7), type: :FU, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 5, y: 7), type: :FU, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 6, y: 7), type: :FU, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 7, y: 7), type: :FU, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 8, y: 7), type: :FU, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 9, y: 7), type: :FU, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 2, y: 8), type: :HISHA, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 8, y: 8), type: :KAKU, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 1, y: 9), type: :KYOU, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 2, y: 9), type: :KEI, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 3, y: 9), type: :GIN, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 4, y: 9), type: :KIN, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 5, y: 9), type: :GYOKU, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 6, y: 9), type: :KIN, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 7, y: 9), type: :GIN, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 8, y: 9), type: :KEI, order: :FIRST)
+    ps << Kifu::Piece.new(pos: Kifu::Pos.new(x: 9, y: 9), type: :KYOU, order: :FIRST)
+
+    b = Kifu::Board.new(pieces: ps)
+    b.normalize!
+    b
+  end
+
+  def normalize!
+    pieces.sort!
   end
 end
