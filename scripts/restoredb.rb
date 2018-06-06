d8e559b1ab0c8c1bef7a0f98f99287f4ce9964b6
nada <nada.wagahai@gmail.com>
2018-06-06 11:33:06 +0900

    restoredb

--- scripts/restoredb.rb
+++ scripts/restoredb.rb
@@ -3,12 +3,14 @@
 require './lib/option'
 require './lib/parser'
 require './lib/db/file'
+require './lib/index/file'
 
 def main(args)
   opt = Option.new(args)
   records_dir = "%s/%s" % [opt.data_dir, opt.records_dir]
 
   db = FileDB.new(opt.data_dir + "/db")
+  index = FileIndex.new(opt.data_dir + "/index.yaml")
   Dir["%s/*" % records_dir].each do |file|
     parser = Parser.new
 
@@ -19,6 +21,7 @@ def main(args)
 
     kifu = parser.parse! input
     db.put_kifu(kifu)
+    index.put(kifu)
 
     kifu_id = kifu.kifu_id
     filename = File.basename file
