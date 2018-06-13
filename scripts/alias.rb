43dc138ceab1b54833ab85569e10764c0c491321
nada <nada.wagahai@gmail.com>
2018-06-13 21:27:10 +0900

    delete file index

--- scripts/alias.rb
+++ scripts/alias.rb
@@ -3,14 +3,12 @@
 require './lib/option'
 require './lib/parser'
 require './lib/db/file'
-require './lib/index/file'
 
 def main(args)
   opt = Option.new(args)
   records_dir = "%s/%s" % [opt.data_dir, opt.records_dir]
 
   db = FileDB.new(opt.data_dir + "/db")
-  index = FileIndex.new(opt.data_dir + "/index.yaml")
   Dir["%s/*" % records_dir].each do |file|
     parser = Parser.new
 
